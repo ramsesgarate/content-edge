0d30a7d05e76ae9ddf3bcee68201e39a1ff0032e
Ramses Garate <47763122+ramsesgarate@users.noreply.github.com>
2022-12-02 11:48:40 -0300

    chore: add nuxt-critters

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', 'nuxt-windicss'],
+  modules: ['@nuxt/content', 'nuxt-windicss', '@nuxtjs/critters'],
   nitro: {
     prerender: {
       crawlLinks: true,
