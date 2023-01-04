d100dc697d55703894431ccaf51b6b1c9d0cf865
Ramses Garate <47763122+ramsesgarate@users.noreply.github.com>
2023-01-04 08:47:04 -0300

    feat: add routeRules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['nuxt-icon', 'nuxt-windicss', '@nuxtjs/critters', '@nuxt/content'],
+  routeRules: {
+    '/blog/**': { swr: true },
+    '/api/**': { cors: true },
+  },
   nitro: {
     prerender: {
       crawlLinks: true,
